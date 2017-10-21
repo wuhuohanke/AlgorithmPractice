c5435a7c2b6166ad832af4934c7b6d752f06e21d
wuhuohanke <wuhuohanke@hotmail.com>
2017-10-21 20:56:40 +0800

    Fix bug of substring and exception issue
    Fix the bug of substring at wrong location. Also add the exception handling while exceeding integer maximum/minimum.

--- LeetCode/src/impl/ReverseInteger.java
+++ LeetCode/src/impl/ReverseInteger.java
@@ -1,30 +1,46 @@
 package impl;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ReverseInteger {
-	public int reverse(int x) {
+	public int reverse(int x) throws InputMismatchException {
 		String input = new String();
 		String tmp = new String();
 		//tmp = null;
-		input.valueOf(x);
+		input = x + "";
 		int result = 0;
 		int len = input.length();
-		if(input.substring(0, 1) == "-"){
-			tmp += input.substring(0,1);
-			for(int i = len; i > 1; i--){
-				tmp += input.substring(i - 1, i);
-			}
+//		String firstTmp = input.substring(0,1);
+//		System.out.println(firstTmp);
+		if(x > Integer.MAX_VALUE || x < Integer.MIN_VALUE){
+			throw new InputMismatchException();	
 		}else{
-			for(int i = len; i > 0; i--){
-				tmp += input.substring(i - 1, i);
+			if("-".equals(input.substring(0, 1))){
+				tmp += input.substring(0,1);
+				for(int i = len; i > 1; i--){
+					tmp += input.substring(i - 1, i);
+				}
+			}else{
+				for(int i = len; i > 1; i--){
+					tmp += input.substring(i - 1, i);
+				}
 			}
-		}
-		result = Integer.parseInt(tmp);
-		return result;
+			result = Integer.parseInt(tmp);
+			return result;
+		}	
     }
 	
 	public static void main(String[] args) {
 		ReverseInteger ri = new ReverseInteger();
-		int result = ri.reverse(-123);
-		System.out.println(result);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input an integer, we will reverse it.");
+		try {
+			int input = scan.nextInt();		
+			System.out.println(ri.reverse(input));
+		} catch (InputMismatchException e) {
+			// TODO Auto-generated catch block
+			System.out.println(0);
+		}
 	}
 }
